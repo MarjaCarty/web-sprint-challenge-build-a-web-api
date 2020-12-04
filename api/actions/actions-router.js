b01d99b779fd341f4ef01609dae9cae2995caa77
MarjaCarty <marja.d.carty@gmail.com>
2020-12-04 09:24:14 -0600

    Add get/post endpoints in actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,34 @@
 // Write your "actions" router here!
 const express = require("express");
+const Action = require("./actions-model");
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const actions = await Action.get();
+    res.status(200).json(actions);
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error retrieving actions",
+      error: err.message,
+    });
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    const newAction = await Action.insert(req.body);
+    res.status(201).json(newAction);
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error creating action",
+      error: err.message,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {});
 
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,4 +3,13 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {});
+router.post("/", async (req, res) => {});
+
+router.get("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {});
+
+router.get("/:id/actions", async (req, res) => {});
+
 module.exports = router;
